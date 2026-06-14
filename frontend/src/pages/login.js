06dbdc505dd0328851fbf7afdfc5bde157f2ad43
anjali-2530 <0372anjali@gmail.com>
2026-06-14 22:42:12 +0530

    Updated login API URL

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -22,7 +22,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/v1/auth/login",
+        "https://loginapp-backend-2chc.onrender.com",
         formData
       );
 
